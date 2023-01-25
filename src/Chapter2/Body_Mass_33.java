20b0b17c3d519030db23f0be8ded277ba41314fd
alinademeter <demeteralina@yahoo.com>
2023-01-25 16:40:08 +0200

    test

--- src/Chapter2/Body_Mass_33.java
+++ src/Chapter2/Body_Mass_33.java
@@ -3,17 +3,17 @@ package Chapter2;
 import java.util.Scanner;
 
 public class Body_Mass_33 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-    int weightinKilograms;
-    int heightinMeters;
-    int bodyMass;
+        int weightinKilograms;
+        int heightinMeters;
+        int bodyMass;
 
-    System.out.print("Enter your weight: ");
-    weightinKilograms = input.nextInt();
-    System.out.print("Enter your height: ");
-    heightinMeters = input.nextInt();
-    System.out.print();
+        System.out.print("Enter your weight: ");
+        weightinKilograms = input.nextInt();
+        System.out.print("Enter your height: ");
+        heightinMeters = input.nextInt();
+        System.out.print();
 
     }
-}// test
+}

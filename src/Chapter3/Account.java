d9e9996bf661103df7e348ee77cab731bada236c
alinademeter <demeteralina@yahoo.com>
2023-01-28 17:06:14 +0200

    Test

--- src/Chapter3/Account.java
+++ src/Chapter3/Account.java
@@ -1,16 +1,37 @@
 package Chapter3;
 
 public class Account {
-    private String name; // instance variable
-
-    //method to set the name in the project
-    public void setName(String name)
+    private String name;
+    private double balance;
+    public Account(String name, double balance)
     {
-        this.name = name; // store the name
+        this.name = name;
+        if (balance > 0.0)
+            this.balance = balance;
     }
-    // method to retrieve the name from the object
+    public void deposit(double depositAmount)
+    {
+        if (depositAmount > 0.0)
+            balance = balance - depositAmount;
+    }
+    public void withdraw (double withdrawl)
+    {
+        if (withdrawl > balance){
+            System.out.println("withdrawl amount is exceeded account balance");
+            return;
+        }
+        balance = balance - withdrawl;
+    }
+    public double getBalance()
+    {
+        return balance;
+    }
+    public void setName(String name)
+ {
+     this.name = name;
+         }
     public String getName()
     {
-        return name; // return the value of name to caller
-    } // end class Account
+        return name;
+    }
 }
